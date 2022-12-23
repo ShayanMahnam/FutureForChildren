152246ccaa9589926b19af56ce480b9233c44858
hkuhku609 <107769663+hkuhku609@users.noreply.github.com>
2022-12-23 12:33:54 +0000

    completed:desktop mode

--- script.js
+++ script.js
@@ -5,13 +5,24 @@ window.onscroll = function () {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-    document.getElementById("header").style.backgroundColor = "var(--yellow)";
-    document.getElementById("bun").style.backgroundColor = "black";
-    document.getElementById("bun2").style.backgroundColor = "black";
+  //if>600px=desktop-mode, bgc use yellow. (by Susan)
+  if (window.screen.width < 601) {
+    if (
+      document.body.scrollTop > 400 ||
+      document.documentElement.scrollTop > 400
+    ) {
+      document.getElementById('mobile-header').style.backgroundColor =
+        'var(--yellow)';
+      document.getElementById('bun').style.backgroundColor = 'black';
+      document.getElementById('bun2').style.backgroundColor = 'black';
+    } else {
+      document.getElementById('mobile-header').style.backgroundColor =
+        'transparent';
+      document.getElementById('bun').style.backgroundColor = 'var(--red)';
+      document.getElementById('bun2').style.backgroundColor = 'var(--red)';
+    }
   } else {
-    document.getElementById("header").style.backgroundColor = "transparent";
-    document.getElementById("bun").style.backgroundColor = "var(--red)";
-    document.getElementById("bun2").style.backgroundColor = "var(--red)";
+    document.getElementById('desktop-header').style.backgroundColor =
+      'var(--yellow)';
   }
 }
